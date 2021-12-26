fe767c2eebcf33eaf9dec92d96900e0aabc672ea
axmetthecoder12 <70323856+axmetthecoder12@users.noreply.github.com>
2021-12-26 19:19:00 +0300

    dasda
    dsadsadsa

---
+++ README.md
@@ -0,0 +1,6 @@
+Bu altpapı, 
+
+Krom.js#0516
+MustafaMert#9440
+ve
+Croxy ^^#6978  TARAFINDAN YAPILMIŞTIR!
---
+++ komutlar/yardım.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js")
+
+exports.run = async (client, message, args) => {
+
+
+const embed = new Discord.MessageEmbed()
+.addField("Yardım Menüsü", `
+ ╔══════════════════════════════════════╗
+ ║
+ ║<:uye:823577810383667200> **•** \`!başvuru-ayar\` ➡ **Başvuru ayarlarını yaparsınız.**
+ ║<:uye:823577810383667200> **•** \`!başvuru-onay\` ➡ **Başvuru yapan birinin başvurusunu onaylarsınız.**
+ ║<:uye:823577810383667200> **•** \`!başvuru-reddet\` ➡ **Başvuru yapan birinin başvurusunu reddedersiniz.**
+ ║<:uye:823577810383667200> **•** \`!başvuru\` ➡ **Başvuru yaparsınız.**
+ ║
+ ╚══════════════════════════════════════╝
+ \`\`\`Altyapı Narcos Code'ye Aittir.\`\`\`
+ `);
+
+
+
+message.channel.send(embed)
+ 
+
+
+
+}
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: ['help'],
+    permLevel: 0
+}
+
+exports.help = {
+    name: 'yardım',
+    description: 'Yardım menüsü.',
+    usage: 'yardım'
+}
